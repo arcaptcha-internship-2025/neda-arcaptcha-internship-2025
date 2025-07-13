8824183428d9334cf6bda331f5e144c0197ae30b
nda <neda.z2003@gmail.com>
2025-07-13 09:56:22 +0330

    feat: handling different commands with cobra

--- project/cmd/start.go
+++ project/cmd/start.go
@@ -1,1 +1,30 @@
 package cmd
+
+import (
+	"log"
+
+	"github.com/nedaZarei/arcaptcha-internship-2025/neda-arcaptcha-internship-2025/config"
+	"github.com/nedaZarei/arcaptcha-internship-2025/neda-arcaptcha-internship-2025/internal/http"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	startCmd = &cobra.Command{
+		Use:   "start",
+		Short: "Start the Apartment service",
+		Run:   start,
+	}
+)
+
+func start(_ *cobra.Command, _ []string) {
+	cfg, err := config.InitConfig("config.yaml")
+	if err != nil {
+		log.Fatalf("failed to initialize config: %v", err)
+	}
+	httpService := http.NewApartmantService(cfg)
+	if err := httpService.Start("Apartment Service"); err != nil {
+		log.Fatalf("failed to start apartment service: %v", err)
+	}
+	httpService.WaitForShutdown()
+}
