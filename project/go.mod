8f8447eb56c5783654106ae6387d06e7e4efc877
nda <neda.z2003@gmail.com>
2025-07-16 19:16:33 +0330

    test: test authorization for jwt auth middleware

--- project/go.mod
+++ project/go.mod
@@ -7,15 +7,17 @@ require (
 	github.com/minio/minio-go/v7 v7.0.94
 	github.com/redis/go-redis/v9 v9.11.0
 	github.com/spf13/cobra v1.9.1
+	github.com/stretchr/testify v1.10.0
 )
 
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
-	github.com/stretchr/testify v1.10.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
 
---
+++ project/internal/http/middleware/auth_test.go
@@ -0,0 +1,66 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/nedaZarei/arcaptcha-internship-2025/neda-arcaptcha-internship-2025/internal/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJWTAuthMiddleware(t *testing.T) {
+	//a valid token
+	validToken, _ := GenerateToken("123", models.Manager)
+
+	tests := []struct {
+		name           string
+		setupRequest   func() *http.Request
+		expectedStatus int
+		expectedUserID string
+	}{
+		{
+			name: "valid token",
+			setupRequest: func() *http.Request {
+				req := httptest.NewRequest("GET", "/", nil)
+				req.Header.Set("Authorization", "Bearer "+validToken)
+				return req
+			},
+			expectedStatus: http.StatusOK,
+			expectedUserID: "123",
+		},
+		{
+			name: "missing token",
+			setupRequest: func() *http.Request {
+				return httptest.NewRequest("GET", "/", nil)
+			},
+			expectedStatus: http.StatusUnauthorized,
+		},
+		{
+			name: "invalid token",
+			setupRequest: func() *http.Request {
+				req := httptest.NewRequest("GET", "/", nil)
+				req.Header.Set("Authorization", "Bearer invalidtoken")
+				return req
+			},
+			expectedStatus: http.StatusUnauthorized,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			handler := JWTAuthMiddleware(models.Manager)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				userID := r.Context().Value(UserIDKey)
+				if tt.expectedUserID != "" {
+					assert.Equal(t, tt.expectedUserID, userID)
+				}
+				w.WriteHeader(http.StatusOK)
+			}))
+
+			w := httptest.NewRecorder()
+			handler.ServeHTTP(w, tt.setupRequest())
+
+			assert.Equal(t, tt.expectedStatus, w.Code)
+		})
+	}
+}
