50c126811584445888bc62fe0632429153ce4d92
nda <neda.z2003@gmail.com>
2025-08-08 00:02:09 +0330

    fix: user_apartment repo mock and test

--- project/internal/repositories/user_apartment_repo_mock.go
+++ project/internal/repositories/user_apartment_repo_mock.go
@@ -7,46 +7,55 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-type mockUserApartmentRepo struct {
+type MockUserApartmentRepository struct {
 	mock.Mock
 }
 
-func (m *mockUserApartmentRepo) CreateUserApartment(ctx context.Context, userApartment models.User_apartment) error {
+func (m *MockUserApartmentRepository) CreateUserApartment(ctx context.Context, userApartment models.User_apartment) error {
 	args := m.Called(ctx, userApartment)
 	return args.Error(0)
 }
 
-func (m *mockUserApartmentRepo) GetResidentsInApartment(apartmentID int) ([]models.User, error) {
+func (m *MockUserApartmentRepository) GetResidentsInApartment(apartmentID int) ([]models.User, error) {
 	args := m.Called(apartmentID)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
 	return args.Get(0).([]models.User), args.Error(1)
 }
 
-func (m *mockUserApartmentRepo) GetUserApartmentByID(userID, apartmentID int) (*models.User_apartment, error) {
+func (m *MockUserApartmentRepository) GetUserApartmentByID(userID, apartmentID int) (*models.User_apartment, error) {
 	args := m.Called(userID, apartmentID)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
 	return args.Get(0).(*models.User_apartment), args.Error(1)
 }
 
-func (m *mockUserApartmentRepo) UpdateUserApartment(ctx context.Context, userApartment models.User_apartment) error {
+func (m *MockUserApartmentRepository) UpdateUserApartment(ctx context.Context, userApartment models.User_apartment) error {
 	args := m.Called(ctx, userApartment)
 	return args.Error(0)
 }
 
-func (m *mockUserApartmentRepo) DeleteUserApartment(userID, apartmentID int) error {
+func (m *MockUserApartmentRepository) DeleteUserApartment(userID, apartmentID int) error {
 	args := m.Called(userID, apartmentID)
 	return args.Error(0)
 }
 
-func (m *mockUserApartmentRepo) GetAllApartmentsForAResident(residentID int) ([]models.Apartment, error) {
+func (m *MockUserApartmentRepository) GetAllApartmentsForAResident(residentID int) ([]models.Apartment, error) {
 	args := m.Called(residentID)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
 	return args.Get(0).([]models.Apartment), args.Error(1)
 }
 
-func (m *mockUserApartmentRepo) IsUserManagerOfApartment(ctx context.Context, userID, apartmentID int) (bool, error) {
+func (m *MockUserApartmentRepository) IsUserManagerOfApartment(ctx context.Context, userID, apartmentID int) (bool, error) {
 	args := m.Called(ctx, userID, apartmentID)
 	return args.Bool(0), args.Error(1)
 }
 
-func (m *mockUserApartmentRepo) IsUserInApartment(ctx context.Context, userID, apartmentID int) (bool, error) {
+func (m *MockUserApartmentRepository) IsUserInApartment(ctx context.Context, userID, apartmentID int) (bool, error) {
 	args := m.Called(ctx, userID, apartmentID)
 	return args.Bool(0), args.Error(1)
 }
